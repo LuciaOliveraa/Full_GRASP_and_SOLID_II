a5f03cddf765e7f43983a15cdae1f1c46f57bfb6
Lucía Olivera <luliolifre@gmail.com>
2023-09-11 01:07:31 -0300

    Ejercicio hecho

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ConsolePrinter
+    {
+        public void PrintRecipe()
+        {
+            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            foreach (Step step in this.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+    }
+}
+
+/* Para asignar la responsabilidad de imprimir la receta a una nueva clase se utilizó el criterio del 
+principio de SRP, una clase solo puede tener una única responsabilidad para enfatizar el dinamismo y versatilidad del programa. */
